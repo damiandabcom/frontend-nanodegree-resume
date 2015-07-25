c059580614706ce717b9bb2413a7fb0d93a88abd
Damian Dabrowski <dabdamian@gmail.com>
2015-07-25 19:36:32 +0200

    refactor: first changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var name = "Damian Dabrowski";
+var role = "Front-End Developer";
+
+var formattedname = HTMLheaderName.replace("%data%", name);
+var formattedrole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedrole);
+$("#header").prepend(formattedname);
